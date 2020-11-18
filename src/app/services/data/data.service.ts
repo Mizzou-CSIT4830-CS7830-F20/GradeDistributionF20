4eb953609a91c29b02c7c38292613ea80f12ae3f
Professor Wergeles <wergelesn@missouri.edu>
2020-11-18 02:53:50 -0600

    Use your own URLs and Firebase variable

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -75,8 +75,7 @@ export class DataService {
 
   addProfessorData(professorName: string): Promise<string> {
     return new Promise<string>(async (resolve, reject) => {
-      const url =
-        'https://us-central1-gradedistributionappf20.cloudfunctions.net/GetProfessorDataF20';
+      const url = 'put your URL to cloud function here';
 
       this.afAuth.currentUser.then((user) => {
         if (user) {
