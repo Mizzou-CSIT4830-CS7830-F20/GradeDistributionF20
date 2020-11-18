b4282ac8ca2ffad9176ec507dfd75c358bf1dbcb
Professor Wergeles <wergelesn@missouri.edu>
2020-11-18 01:24:24 -0600

    Fixing up the Routing and Testing Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,39 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { Custom404Component } from './pages/custom404/custom404.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { AuthGuardService } from './services/auth/auth-guard.service';
+import { ClassDetailsComponent } from './pages/class-details/class-details.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: DashboardComponent,
+    // canActivate: [AuthGuardService],
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    // canActivate: [AuthGuardService],
+  },
+  {
+    path: 'classDetails/:professorId/:classId',
+    component: ClassDetailsComponent,
+    // canActivate: [AuthGuardService],
   },
   {
     path: '**',
-    component: Custom404Component
-  }
+    component: Custom404Component,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
